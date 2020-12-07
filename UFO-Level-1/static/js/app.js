5031a0bf90c44bbf09eccac8353f8950a1b60945
JeniLam <jenilam639@gmail.com>
2020-12-06 21:53:27 -0600

    Gets data into HTML

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,4 +1,20 @@
 // from data.js
 var tableData = data;
+console.log(tableData)
 
-// YOUR CODE HERE!
+// create reference to table body
+var tbody = d3.select("tbody");
+
+
+// Loop through data array
+tableData.forEach(sighting => {
+    // append one table row for each ufo object
+    var row = tbody.append("tr");
+
+    // use object.entries and forEach to iterate through keys and values (day 2 activity 7 & 8)
+    Object.entries(sighting).forEach(([key,value]) => {
+        // append one cell per  value within data set (table headers in html)
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
