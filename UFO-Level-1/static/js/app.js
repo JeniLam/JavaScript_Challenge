19a626fe0489b9f482ad7735c82373a0f8181225
JeniLam <jenilam639@gmail.com>
2020-12-11 20:21:38 -0600

    Adds notes to level one, starts level 2

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -12,7 +12,7 @@ tableData.forEach(sighting => {
     // append one table row for each ufo object
     var row = tbody.append("tr");
 
-    // use object.entries and forEach to iterate through keys and values (day 2 activity 7 & 8 and Day 3 act 3)
+    // use object.entries and forEach to iterate through keys and values (day 2 activity 7 & 8 and Day 3 act 3) entries gets you both the Key & the value
     Object.entries(sighting).forEach(([key, value]) => {
         // append one cell per  value within data set (table headers in html)
         var cell = row.append("td");
@@ -26,7 +26,7 @@ tableData.forEach(sighting => {
 var filterTableButton = d3.select("#filter-btn");
 console.log(filterTableButton);
 
-// select form
+// select form (from office hours with Mo)
 var form = d3.select("#datetimeform");
 console.log(form);
 
