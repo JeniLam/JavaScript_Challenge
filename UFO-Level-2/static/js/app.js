52d5297d2d8d74c6c696d42cf91e50c5f7f0f467
JeniLam <jenilam639@gmail.com>
2020-12-11 21:29:34 -0600

    Adds selection of button/form & event handler

--- UFO-Level-2/static/js/app.js
+++ UFO-Level-2/static/js/app.js
@@ -20,3 +20,57 @@ tableData.forEach(sighting => {
         cell.text(value);
     });
 });
+
+//*** Use a date form in your HTML document and write JavaScript code that will listen
+// Most of the below copied from Day 3 Activity 9
+// select the button
+var filterTableButton = d3.select("#filter-btn");
+console.log("Selecting Button")
+console.log(filterTableButton);
+
+// select form (from office hours with Mo)
+var form = d3.select("#datetimeform");
+console.log("Selecting Form")
+console.log(form);
+
+// Create event handlers
+filterTableButton.on("click", runEnter);
+form.on("submit", runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // clear the table
+    tbody.html("");
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select(".form-control");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+    
+    // use input on form to filter data by datetime in data.js file per Dan, can chain the filters to get data needed.
+    // data from data.js for reference
+    // datetime: "1/1/2010",
+    // city: "bonita",
+    // state: "ca",
+    // country: "us",
+    // shape: "light",
+    // durationMinutes: "13 minutes",
+    var filteredData = tableData.filter((sighting) => sighting.datetime === inputValue);
+
+    console.log(filteredData);
+
+// Use filtered data to repopulate the table
+   filteredData.forEach(function(filteredItem) {
+       var row = tbody.append("tr");
+       Object.entries(filteredItem).forEach(([key,value]) => {
+           var cell = row.append("td");
+           cell.text(value);
+       });
+   });
+};
